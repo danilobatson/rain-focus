7a3062ddf0404d26b4edb08af80a1fe52e80649c
Danilo Batson <djbatson19@gmail.com>
2022-02-14 21:41:10 -0800

    drawer rendered and table structured
    delete variable

--- src/components/table-details/table-details.component.js
+++ src/components/table-details/table-details.component.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './table-details.styles.scss'
-import { HiPencil } from "react-icons/hi";
-import { FaTrash } from "react-icons/fa";
+import Checkbox from '@mui/material/Checkbox';
+import { purple } from '@mui/material/colors';
 
 
 export const TableDetails = () => {
@@ -20,111 +20,160 @@ export const TableDetails = () => {
 
 
   return (
-  <>
-    <div className='header'>
-        <input type="checkbox" checked={allChecked} onChange={selectAll}  />
-      <span class="checkmark"></span>
-      <span className="sessions">File Name</span>
-      <span className="sessions">File Type</span>
-      <span className="sessions">File Create Date</span>
-      <span className="sessions">Published Status</span>
-      <span className="sessions">Sessions Name</span>
-      <span className="sessions">Sessions Code</span>
-      <span className="sessions">Sessions Status</span>
+    <>
+      <table style={{ width: 'auto'}}>
+        <tr>
+          <th className="sessions" style={{ width: 'auto' }}>
+            <Checkbox
+              className='check'
+              checked={allChecked} onChange={selectAll}
+              sx={{
+                color: purple[800],
+                '&.Mui-checked': {
+                  color: purple[600],
+                },
+                '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+              }}
+            />
+          </th>
 
-
-    </div>
-    <hr className="hr-top" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} onChange={select}/>
-      <span class="checkmark"></span>
-      <span>New Microsoft PowerPoint Presentation_150473403361001aMvB.pptx</span>
-      <span> Presentation  </span>
-      <span> September 06, 2017 | 02:40 PM</span>
-      <span>Not Published</span>
-      <span>Reject Data Integration until Column-oriented databases</span>
-      <span>BR1000</span>
-      <span>Accepted</span>
-    </p>
-    <hr className="hr" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} />
-      <span class="checkmark"></span>
-      <span>Portugal (2)_15121542334140018toT.pptx</span>
-      <span> Presentation  </span>
-      <span> December 01, 2017 | 10:50 AM</span>
-      <span>Not Published</span>
-      <span>Promise Data virtualization for Hadoop</span>
-      <span>BR1003</span>
-      <span>Accepted</span>
-    </p>
-    <hr className="hr" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} />
-      <span class="checkmark"></span>
-      <span>Portugal (2)_1512542334140018toT_1512162178268001wXbS.pptx</span>
-      <span> Presentation  </span>
-      <span> December 01, 2017 | 01:02 PM</span>
-      <span>Not Published</span>
-      <span>Promise Data virtualization for Hadoop</span>
-      <span>BR1003</span>
-      <span>Accepted</span>
-    </p>
-    <hr className="hr" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} />
-      <span class="checkmark"></span>
-      <span>Session Scanning Training.pptx</span>
-      <span> Presentation  </span>
-      <span> April 16, 2018 | 10:36 AM</span>
-      <span>Not Published</span>
-      <span>Reach in-memory data fabric unlike PIG</span>
-      <span>BR1005</span>
-      <span>Accepted</span>
-    </p>
-    <hr className="hr" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} />
-      <span class="checkmark"></span>
-      <span>PPT TEST2_1498603851830001mOeD_1517269636907001D3OS.pptx</span>
-      <span> Presentation  </span>
-      <span> April 16, 2018 | 12:50 PM</span>
-      <span>Not Published</span>
-      <span>Reach in-memory data fabric unlike PIG</span>
-      <span>BR1005</span>
-      <span>Accepted</span>
-    </p>
-    <hr className="hr" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} />
-      <span class="checkmark"></span>
-      <span>New Microsoft PowerPoint Presentation_150473403361001aMvB.pptx</span>
-      <span> Presentation  </span>
-      <span> July 11, 2017 | 07:13 AM</span>
-      <span>Participate Stream analytics throughout SkyTree</span>
-      <span>Not Published</span>
-      <span>BR1019</span>
-      <span>Accepted</span>
-    </p>
-    <hr className="hr" />
-
-    <p className='table-data'>
-        <input type="checkbox" checked={check} />
-      <span class="checkmark"></span>
-      <span>Sweet Preso.pptx</span>
-      <span> Presentation  </span>
-      <span> November 13, 2017 | 02:59 PM</span>
-      <span>Published</span>
-      <span>Add Data intergration upon Column-oriented databases</span>
-      <span>BR1020</span>
-      <span>Accepted</span>
-    </p>
+          <th className="sessions" style={{ width: '10%' }}>File Name</th>
+          <th className="sessions" style={{ width: 'auto' }}>File Type</th>
+          <th className="sessions" style={{ width: '12%' }}>File Create Date</th>
+          <th className="sessions" style={{ width: '6%' }}>Published Status</th>
+          <th className="sessions" style={{ width: 'auto' }}>Sessions Name</th>
+          <th className="sessions" style={{ width: '6%' }}>Sessions Code</th>
+          <th className="sessions" style={{ width: '6%' }}>Sessions Status</th>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />
+          <td>New Microsoft PowerPoint Presentation_150473403361001aMvB.pptx</td>
+          <td> Presentation  </td>
+          <td> September 06, 2017 | 02:40 PM</td>
+          <td>Not Published</td>
+          <td>Reject Data Integration until Column-oriented databases</td>
+          <td>BR1000</td>
+          <td>Accepted</td>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />          <td>Portugal (2)_15121542334140018toT.pptx</td>
+          <td> Presentation  </td>
+          <td> December 01, 2017 | 10:50 AM</td>
+          <td>Not Published</td>
+          <td>Promise Data virtualization for Hadoop</td>
+          <td>BR1003</td>
+          <td>Accepted</td>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />          <td>Portugal (2)_1512542334140018toT_1512162178268001wXbS.pptx</td>
+          <td> Presentation  </td>
+          <td> December 01, 2017 | 01:02 PM</td>
+          <td>Not Published</td>
+          <td>Promise Data virtualization for Hadoop</td>
+          <td>BR1003</td>
+          <td>Accepted</td>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />          <td>Session Scanning Training.pptx</td>
+          <td> Presentation  </td>
+          <td> April 16, 2018 | 10:36 AM</td>
+          <td>Not Published</td>
+          <td>Reach in-memory data fabric unlike PIG</td>
+          <td>BR1005</td>
+          <td>Accepted</td>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />          <td>PPT TEST2_1498603851830001mOeD_1517269636907001D3OS.pptx</td>
+          <td> Presentation  </td>
+          <td> April 16, 2018 | 12:50 PM</td>
+          <td>Not Published</td>
+          <td>Reach in-memory data fabric unlike PIG</td>
+          <td>BR1005</td>
+          <td>Accepted</td>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />          <span class="checkmark"></span>
+          <td>Presentation1_1499782389871001Qvcf.pptx</td>
+          <td> Presentation  </td>
+          <td> July 11, 2017 | 07:13 AM</td>
+          <td>Not Published</td>
+          <td>Participate Stream analytics throughout SkyTree</td>
+          <td>BR1019</td>
+          <td>Accepted</td>
+        </tr>
+        <tr>
+          <Checkbox
+            checked={check} onChange={select}
+            sx={{
+              color: purple[800],
+              '&.Mui-checked': {
+                color: purple[600],
+              },
+              '& .MuiSvgIcon-root': { fontSize: '1.17rem' }
+            }}
+          />          <td>Sweet Preso.pptx</td>
+          <td> Presentation  </td>
+          <td> November 13, 2017 | 02:59 PM</td>
+          <td>Published</td>
+          <td>Add Data intergration upon Column-oriented databases</td>
+          <td>BR1020</td>
+          <td>Accepted</td>
+        </tr>
+      </table>
 
     </>
   )
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import {Card} from "../../components/card/card.component";
 import {IndexCard} from "../../components/index-card/index-card.component";
-
+import Drawer from '@mui/material/Drawer';
+import { purple } from '@mui/material/colors';
+import { HiPencil } from "react-icons/hi";
+import { FaTrash } from "react-icons/fa";
 import { BadgeDetails } from '../../components/badge-details/badge-details.component'
 import { CalendarDetails } from '../../components/calendar-details/calendar-details.component'
 import { TableDetails } from '../../components/table-details/table-details.component'
 
 import './homepage.styles.scss';
 
-const HomePage = () => (
+const HomePage = () => {
+
+  return (
   <div className='homepage'>
+    <Drawer variant="permanent"
+        anchor="left" sx={{
+        '& .MuiDrawer-paper': {
+          width: '7%',
+          boxSizing: 'border-box',
+        },
+        }}>
+        <div style={{display: 'flex', width: '100%', height: '100%', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly', }}>
+        <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+          <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+          <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+          <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+          <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+          <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+          <HiPencil className='edit' size='1.4em' />
+          <FaTrash className='delete' size='1.4em' />
+        </div>
+      </Drawer>
     <Card>
       <BadgeDetails />
     </Card>
@@ -22,6 +51,9 @@ const HomePage = () => (
       <TableDetails />
     </IndexCard>
   </div>
-);
+
+  )
+}
+
 
 export default HomePage;
